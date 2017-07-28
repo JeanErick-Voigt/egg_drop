adfb861f8517efbf53b973b227c5b964d023ec02
Jean-Erick Voigt <voigtjeanerick@gmail.com>
2017-07-28 16:14:06 -0400

    program works with all functions and makefile is operational

--- egg_drop.c
+++ egg_drop.c
@@ -1,12 +1,17 @@
 #include "egg.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+
+int  brute_force(int lowest_number, int num_of_floors, egg *e);
+int educated_split(int maximum_floor, int minimum_floor, int egg_group);
 
 
 int main(int argc, char **argv)
 {
+
 	int num_of_floors, egg_group;
-	int current_floor;
+	//int current_floor;
 	if(argc > 3){
 		printf("Provided too many arguments\n");
 		exit(1);
@@ -18,25 +23,72 @@ int main(int argc, char **argv)
 		num_of_floors = atoi(argv[1]);
 		egg_group = atoi(argv[2]);
 	}
+
+//	num_of_floors = atoi(argv[1]);
+//	egg_group = atoi(argv[2]);
+
 	egg *e = NULL;
-	e = lay_egg();
-	egg_group -= 1;
-	//printf("%lu", sizeof(e));
-	printf("num of floors is %d\n", num_of_floors);
-	//printf("num of eggs is %d", egg_group);
 	e = lay_egg();	
-	//printf("%d", sizeof(e));
 	puts("\n");
+	int guessing = 1;
+	int maximum_floor = num_of_floors;
+	int minimum_floor = 1;
+	int lowest_number = 1;
+	int guess_number = 0;
+	while(guessing){
+		int max_safe_floor;
+		guess_number += 1;
+		int current_floor = educated_split(maximum_floor, minimum_floor, egg_group);
+		if(egg_group > 1){
+			//current_floor;
+			printf("This is current floor multi guess %d\n", current_floor);
+			egg_drop_from_floor(e, current_floor);
+			printf("This is after egg drop\n");
+			if(egg_is_broken(e) == 1){
+				egg_group -= 1;
+				printf("This is number of  eggs %d", egg_group);
+				maximum_floor = current_floor;
+				minimum_floor = lowest_number;
+				e = lay_egg();
+			}else{
+				minimum_floor = lowest_number = current_floor;
+			}
+		}else{
+			//if(brute_force(minimum_floor, maximum_floor)) >= 1){
+				//printf("The maximum safe floor is %d", brute_force(minimum_floor, maximum_floor));
+			max_safe_floor = brute_force(minimum_floor, maximum_floor, e);
+			printf("highest safe floor is %d\n", max_safe_floor - 1);
+			printf("num of guesses = %d\n", guess_number + (max_safe_floor - minimum_floor));
+			guessing = 0;
+		}
+		
+	}
+	printf("exit looop\n");
+	destroy_egg(e);
+	return(0);
+}
+
+
 	// brute force way
-	for(current_floor = 1; current_floor <=  num_of_floors; current_floor++){
+int brute_force(int  lowest_number, int num_of_floors, egg *e){
+	int current_floor;
+	for(current_floor = lowest_number; current_floor <=  num_of_floors; current_floor++){
 		egg_drop_from_floor(e, current_floor);
 		printf("current floor %d\n", current_floor);
 		if(egg_is_broken(e) == 1){
-			printf("Maximum safe floor to drop from is %d floor", current_floor - 1);
-			break;
-		}else{
-			printf("Next floor");
+			//printf("Maximum safe floor to drop from is %d floor", current_floor - 1);
+			//break;
+			return(current_floor);
 		}
 	}
 	return(0);
+
+}
+
+int educated_split(int maximum_floor, int minimum_floor, int egg_group)
+{
+	int range = maximum_floor -(minimum_floor - 1);
+	int guess;
+	guess = (minimum_floor - 1) + floor(range/egg_group);
+	return guess;
 }
--- makefile
+++ makefile
@@ -6,10 +6,15 @@ egg: egg.c egg.h
 	gcc $(CFLAGS) -c egg.c -o egg.o
 
 egg_drop: egg.o egg_drop.c
-	gcc $(CFLAGS) egg.o egg_drop.c -o egg_drop
+	gcc $(CFLAGS) egg.o egg_drop.c -o egg_drop -lm
 
 debug: CFLAGS += -g
 debug: all
 
+profile: CFLAGS += -pg
+profile: all
+
+test: all
+	curl https://raw.githubusercontent.com/tclasen/egg_drop/master/test.sh -o test.sh && chmod 755 test.sh && ./test.sh
 clean:
 	rm -f egg.o egg_drop
