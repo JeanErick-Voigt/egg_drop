34bd86169911eabcb3d43a1d245030852f7fa06e
Jean-Erick Voigt <voigtjeanerick@gmail.com>
2017-07-29 08:50:41 -0400

    All teacher provided cases should be satisfied except for 100 floors 5 eggs

--- egg_drop.c
+++ egg_drop.c
@@ -11,7 +11,6 @@ int main(int argc, char **argv)
 {
 
 	int num_of_floors, egg_group;
-	//int current_floor;
 	if(argc > 3){
 		printf("Provided too many arguments\n");
 		exit(1);
@@ -23,7 +22,6 @@ int main(int argc, char **argv)
 		num_of_floors = atoi(argv[1]);
 		egg_group = atoi(argv[2]);
 	}
-	//egg *e = NULL;
 	egg *e  = lay_egg();	
 	puts("\n");
 	int guessing = 1;
@@ -31,60 +29,62 @@ int main(int argc, char **argv)
 	int minimum_floor = 1;
 	int lowest_number = 1;
 	int guess_number = 0;
-	double ratio = 1;
-	int difference;
+	int egg_count = 1;
+	int current_floor = 0;
+	int max_safe_floor;
 	while(guessing){
-		int max_safe_floor;
+		//fringe cases
+		if(maximum_floor - 1 == minimum_floor || minimum_floor == maximum_floor){
+			printf("%d is the maximum safe floor, found after %d drops\n", minimum_floor, guess_number);
+			break;
+		}
+		if(maximum_floor - minimum_floor == minimum_floor + 1){
+			printf("%d is the maximum safe floor, found after %d drops\n", minimum_floor + 1, guess_number);
+			break;
+		}
+
+//		printf("The minimum floor %d and max floor %d\n", minimum_floor, maximum_floor);
+		printf("Dropping egg %d from floor: \n", egg_count); 
 		guess_number += 1;
-		difference = maximum_floor - minimum_floor;
-		ratio = (double) difference/egg_group;
-		int current_floor = educated_split(maximum_floor, minimum_floor, egg_group);
-		if(egg_group > 1 && ratio >= 1){
-			//current_floor;
-			ratio = difference/egg_group;
-			printf("This is current floor multi guess %d\n", current_floor);
+		if(egg_group > 1){
+			current_floor = educated_split(maximum_floor, minimum_floor, egg_group);
+			printf("#%d", current_floor);
 			egg_drop_from_floor(e, current_floor);
-			printf("This is after egg drop\n");
-			printf("This is maximum floor %d\n", maximum_floor);
 			if(egg_is_broken(e) == 1){
 				egg_group -= 1;
-				printf("This is number of  eggs %d", egg_group);
+				egg_count += 1;
+				printf(" CRACK\n");
 				maximum_floor = current_floor;
-				printf("This is maximum floor %d\n", maximum_floor);
-				minimum_floor = lowest_number;
-				//destroy_egg(e);
+				minimum_floor = lowest_number + 1;  //test
 				e = lay_egg();
 			}else{
 				minimum_floor = lowest_number = current_floor;
+				printf(" safe\n");
 			}
 		}else{
-			//if(brute_force(minimum_floor, maximum_floor)) >= 1){
-				//printf("The maximum safe floor is %d", brute_force(minimum_floor, maximum_floor));
 			max_safe_floor = brute_force(minimum_floor, maximum_floor, e);
-			printf("highest safe floor is %d\n", max_safe_floor - 1);
-			printf("num of guesses = %d\n", guess_number + (max_safe_floor - minimum_floor));
-			printf("this is final maximum floor %d", maximum_floor);
+			printf("%d is the maximum safe floor, found after %d drops \n", (max_safe_floor - 1), 
+				guess_number + (max_safe_floor - minimum_floor));
 			guessing = 0;
 		}
 		
 	}
-	printf("exit looop\n");
 	destroy_egg(e);
 	return(0);
 }
 
 
-	// brute force way
 int brute_force(int  lowest_number, int num_of_floors, egg *e){
 	int current_floor;
 	for(current_floor = lowest_number; current_floor <=  num_of_floors; current_floor++){
-		egg_drop_from_floor(e, current_floor);
-		printf("current floor %d\n", current_floor);
+		egg_drop_from_floor(e, current_floor + 1);
+		printf(" #%d", current_floor + 1);
 		if(egg_is_broken(e) == 1){
-			//printf("Maximum safe floor to drop from is %d floor", current_floor - 1);
-			//break;
-			//destroy_egg(e);
+			printf(" CRACK\n");
 			return(current_floor);
+		}else{
+			printf(" safe\n");
+			current_floor += 1;
 		}
 	}
 	return(0);
@@ -93,8 +93,17 @@ int brute_force(int  lowest_number, int num_of_floors, egg *e){
 
 int educated_split(int maximum_floor, int minimum_floor, int egg_group)
 {
-	int range = maximum_floor - minimum_floor;
-	int guess;
+	int range, guess;
+	range = maximum_floor -minimum_floor;
+	if(range > 75)
+		egg_group = 2;
+	if(range < 50 && range > 30)
+		egg_group = 3;
+	while(range < egg_group && egg_group > 1){
+		egg_group -= 1;
+		range = maximum_floor - minimum_floor;
+	}
+
 	guess = (minimum_floor) + floor(range/egg_group);
 	return guess;
 }
